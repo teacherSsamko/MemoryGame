427d709da71a3e7c841cab73cf76a653d7b5e291
ssamko <teacher.ssamko@gmail.com>
2021-01-17 00:36:22 +0900

    init single page application

--- MemoryGame/ContentView.swift
+++ MemoryGame/ContentView.swift
@@ -1,21 +1,51 @@
 //
 //  ContentView.swift
-//  MemoryGame
+//  Memorize
 //
-//  Created by lee eunseop on 2021/01/17.
+//  Created by lee eunseop on 2021/01/03.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    var viewModel: EmojiMemoryGame
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack() {
+            ForEach(0..<4) { idx in
+                CardView(card: ...)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.orange)
+        .font(Font.largeTitle)
     }
 }
 
+struct CardView: View {
+    var card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack() {
+            if card.isFaceup {
+                RoundedRectangle(cornerRadius: 10.0)
+                    .fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0)
+                    .stroke(lineWidth: 3.0)
+                    
+                Text(card.content)
+                    .font(.largeTitle)
+            } else {
+            RoundedRectangle(cornerRadius: 10.0)
+                .fill()
+            }
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(viewModel: EmojiMemoryGame())
     }
 }
